e8f2127a631d7bfc1fde57e94ca5f2483e6f6554
NathanHarbour <nathanharbour@gmail.com>
2021-01-21 21:56:33 -0600

    Initial submission

---
+++ Source/CM_Chill_Out/ChillOutMod.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CM_Chill_Out
+{
+    public class ChillOutMod : Mod
+    {
+        private static ChillOutMod _instance;
+        public static ChillOutMod Instance => _instance;
+
+        public ChillOutMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("CM_Chill_Out");
+            harmony.PatchAll();
+
+            _instance = this;
+        }
+    }
+}
